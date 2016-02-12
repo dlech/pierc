800695f8bd38354dfc497f7ec7d792ed0324ec87
David Lechner <david@lechnology.com>
2016-02-11 19:48:54 -0600

    Change how files are downloaded.
    Now checks debian/incoming, raspbian/incoming and ubuntu/incoming directories.
    This allows files to be downloaded directly in to reprepro incoming directories.
    
    Also, make sure the .changes files are downloaded last. This makes sure that
    reprepro is not triggered until all of the files are downloaded.

--- bot/pierc.py
+++ bot/pierc.py
@@ -8,7 +8,6 @@ import re
 import time
 import datetime
 import easywebdav
-import subprocess
 
 # mine
 import piercdb
@@ -160,29 +159,38 @@ class Logger(irclib.SimpleIRCClient):
         if verbose:
             self.send_msg(connection, "checking for incoming debian packages...")
         try:
-            files = self.webdav.ls('/debian')
-            if len(files) == 1:
+            files = self.webdav.ls('debian/incoming') + \
+                    self.webdav.ls('raspbian/incoming') + \
+                    self.webdav.ls('ubuntu/incoming')
+            # each ls returns an item for the directory, so if there are only 3 files, the directories are empty
+            if len(files) <= 3:
                 if verbose:
                     self.send_msg(connection, "No files to download.")
                 return
+
             self.send_msg(connection, "Found some incoming packages...")
             changes_files = []
+
+            def download(name):
+                local_file = self.webdav_download_dir + name
+                self.send_msg(connection, "downloading {}".format(name))
+                self.webdav.download(name, local_file)
+                print("downloaded {}".format(local_file))
+                self.webdav.delete(name)
+
             for f in files:
                 if f.contenttype == 'httpd/unix-directory':
                     continue
-                self.send_msg(connection, "downloading " + f.name)
-                local_file = f.name.replace('/debian', self.webdav_download_dir)
-                if local_file[-8:] == '.changes':
-                    changes_files.append(local_file)
-                self.webdav.download(f.name, local_file)
-                print("downloaded {}".format(local_file))
-                self.webdav.delete(f.name)
+                if f.name[-8:] == '.changes':
+                    changes_files.append(f)
+                else:
+                    # download .changes files last because they trigger the handler on the server
+                    download(f.name)
             if len(changes_files) == 0:
                 self.send_msg(connection, "No .changes files found.")
                 return
             for f in changes_files:
-                self.send_msg(connection, "submitting " + f.split('/')[-1])
-                subprocess.check_call(['dput', 'ev3dev.org', f])
+                download(f.name)
             self.send_msg(connection, "Done! You should receive an email soon.")
         except Exception, e:
             self.send_msg(connection, e)
